c3a3b78298b510e85feef2cfd0d527a6760b5ebc
pang <pysche@ipbfans.org>
2013-09-24 21:41:25 +0800

    updated

--- application/modules/api/controllers/MessageController.php
+++ application/modules/api/controllers/MessageController.php
@@ -3,13 +3,36 @@
 class Api_MessageController extends Lja_Controller_Action_Api {
 
 	public function indexAction() {
+		$method = $this->getRequest()->getMethod();
+
+		switch ($method) {
+			case 'GET':
+				$this->siteValidate();
+				break;
+			case 'POST':
+				$this->processMessage();
+				break;
+			default:
+				exit;
+				break;
+		}
+	}
+
+	protected function siteValidate() {
 		$signature = $this->getRequest()->getParam('signature', '');	//	微信加密签名
 		$timestamp = $this->getRequest()->getParam('timestamp', '');	//	时间戳
 		$nonce = $this->getRequest()->getParam('nonce', '');			//	随机数
 		$echostr = $this->getRequest()->getParam('echostr', '');		//	随机字符串
 
 		$sign = Lja_Weixin::sign($timestamp, $nonce);
-		
-		die($sign==$signature ? $echostr : 'forbidden');
+
+		echo $sign==$signature ? $echostr : 'forbidden';
+		exit;
+	}
+
+	protected function processMessage() {
+		Lja_Log::i()->debug(var_export($_POST, true));
+
+		die("OK");
 	}
 }
