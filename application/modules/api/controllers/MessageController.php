491a41d78d230995a3f796a2a21eef5a04aa5593
pang <pysche@ipbfans.org>
2013-09-24 21:32:40 +0800

    消息接口签名校验

--- application/modules/api/controllers/MessageController.php
+++ application/modules/api/controllers/MessageController.php
@@ -8,7 +8,8 @@ class Api_MessageController extends Lja_Controller_Action_Api {
 		$nonce = $this->getRequest()->getParam('nonce', '');			//	随机数
 		$echostr = $this->getRequest()->getParam('echostr', '');		//	随机字符串
 
-		Lja_Log::i()->debug($signature.'-'.$timestamp.'-'.$nonce.'-'.$echostr.'-'.Lja_Weixin::sign($timestamp, $nonce));
-		die($echostr);
+		$sign = Lja_Weixin::sign($timestamp, $nonce);
+		
+		die($sign==$signature ? $echostr : 'forbidden');
 	}
 }
