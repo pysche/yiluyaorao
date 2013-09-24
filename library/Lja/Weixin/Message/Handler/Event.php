39d016b759ff78d346d630e43fde88024cef9063
pang <pysche@ipbfans.org>
2013-09-24 23:15:20 +0800

    updated

--- library/Lja/Weixin/Message/Handler/Event.php
+++ library/Lja/Weixin/Message/Handler/Event.php
@@ -26,7 +26,7 @@ class Lja_Weixin_Message_Handler_Event extends Lja_Weixin_Message_Handler_Base {
 					<Content><![CDATA[%s]]></Content>
 					<FuncFlag>0</FuncFlag>
 					</xml>";  
-		$xml = sprintf($xml, $this->msg->FromUserName, $this->msg->ToUserName, time(), 'text', '感谢您的关注');
+		$xml = sprintf($xml, $this->msg->FromUserName, $this->msg->ToUserName, time(), 'text', '您好，欢迎关注“一路妖娆**苏州外贸”的微信帐号！本店淘宝店铺网址为：http://shop33487592.taobao.com/');
 		Lja_Log::i()->debug($xml);
 
 		header('Content-Type: text/xml; charset=utf-8');
