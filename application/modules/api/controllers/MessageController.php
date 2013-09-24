75a14b0a8bf39fca81555bca5862cecfde130201
pang <pysche@ipbfans.org>
2013-09-24 21:19:52 +0800

    updated

---
+++ application/modules/api/controllers/MessageController.php
@@ -0,0 +1,13 @@
+<?php
+
+class Api_MessageController extends Lja_Controller_Action_Api {
+
+	public function indexAction() {
+		$signature = $this->getRequest()->getParam('signature', '');	//	微信加密签名
+		$timestamp = $this->getRequest()->getParam('timestamp', '');	//	时间戳
+		$nonce = $this->getRequest()->getParam('nonce', '');			//	随机数
+		$echostr = $this->getRequest()->getParam('echostr', '');		//	随机字符串
+
+		die('ok<HR>');
+	}
+}
